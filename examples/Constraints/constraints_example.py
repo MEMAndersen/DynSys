c9b87eb52120f9c5af7db3075c25db54c044ff1c
Richard Hollamby <rihy@cowi.com>
2018-04-13 08:59:58 +0100

    Constraints example added

---
+++ examples/Constraints/constraints_example.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Example to demonstrate (and test) use of constraints
+"""
+
+# dynsys library imports
+import modalsys
+
+
+# Define a modal system
+my_modal_sys = modalsys.ModalSys()
+my_modal_sys.PrintSystemMatrices()
+
+
