5226de37e3bf6e572321ee526bdba6276a4d6961
= <rihy@cowi.com>
2019-07-08 16:49:23 +0100

    pre-release commit

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,7 +8,7 @@ and this project adheres to:
 
 ***
 
-## Unreleased
+## [1.8.0] - 2019-07-08 - RIHY
 
 ### Added
 
--- dynsys/__init__.py
+++ dynsys/__init__.py
@@ -1,5 +1,5 @@
 #
 
-__version__ = "1.7.5"
+__version__ = "1.8.0"
 
 __author__ = "Richard Hollamby (RIHY), COWI UK Bridge"
